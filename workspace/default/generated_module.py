70cef9476a5a550644852400b5d398d67f94636f
bahattinyunus <bahattinyunuscetin1@gmail.com>
2026-03-24 15:58:29 +0300

    update

---
+++ workspace/default/generated_module.py
@@ -0,0 +1,10 @@
+
+import jwt
+from datetime import datetime, timedelta
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, "SECRET_KEY", algorithm="HS256")
+    return encoded_jwt
